34469954ce88a4432f8160a3c0177601f7bf7cec
Luca Geretti <luca.geretti@gmail.com>
2021-03-01 16:25:27 +0100

    Replace references to original project and author

--- source/replaceme.cpp
+++ source/replaceme.cpp
@@ -1,31 +1,31 @@
 /***************************************************************************
  *            replaceme.cpp
  *
- *  Copyright  2021  Luigi Capogrosso
+ *  Copyright  2021  Mirco De Marchi
  *
  ****************************************************************************/
 
 /*
- *  This file is part of PBDD.
+ *  This file is part of Ariadne.
  *
- *  PBDD is free software: you can redistribute it and/or modify
+ *  Ariadne is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  PBDD is distributed in the hope that it will be useful,
+ *  Ariadne is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with PBDD.  If not, see <https://www.gnu.org/licenses/>.
+ *  along with Ariadne.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-namespace PBDD {
+namespace Ariadne {
 
 int replaceme(int v) {
     return v*v;
 }
 
-} // namespace PBDD
+} // namespace Ariadne
--- tests/test_replaceme.cpp
+++ tests/test_replaceme.cpp
@@ -1,32 +1,32 @@
 /***************************************************************************
  *            tests/test_replaceme.cpp
  *
- *  Copyright  2021  Luigi Capogrosso
+ *  Copyright  2021  Mirco De Marchi
  *
  ****************************************************************************/
 
 /*
- *  This file is part of PBDD.
+ *  This file is part of Ariadne.
  *
- *  PBDD is free software: you can redistribute it and/or modify
+ *  Ariadne is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  PBDD is distributed in the hope that it will be useful,
+ *  Ariadne is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with PBDD.  If not, see <https://www.gnu.org/licenses/>.
+ *  along with Ariadne.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "test.hpp"
 #include "replaceme.hpp"
 
 using namespace std;
-using namespace PBDD;
+using namespace Ariadne;
 
 class TestReplaceMe {
   public:
