8a9bbe85753d1d9839f817815aaf628f68046fad
Mirco De Marchi <mirco.demarchi@studenti.univr.it>
2021-05-30 19:28:15 +0200

    Create parser module and csv empty sources

---
+++ source/parser/csv.hpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *            csv.hpp
+ *
+ *  Copyright  2021  Mirco De Marchi
+ *
+ ****************************************************************************/
+
+/*
+ *  This file is part of Ariadne.
+ *
+ *  Ariadne is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Ariadne is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Ariadne.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*! \file csv.hpp
+ *  \brief CSV Parser header.
+ */
+
+#ifndef ARIADNE_PARSER_CSV_HPP
+#define ARIADNE_PARSER_CSV_HPP
+
+namespace Ariadne {
+
+
+
+} // namespace Ariadne
+
+#endif // ARIADNE_REPLACEME_HPP
