4e93fa27c74b39b14619f964c7d84982afd92154
Mirco De Marchi <mirco.demarchi@studenti.univr.it>
2021-05-31 16:40:39 +0200

    Implement base CSV interpreter

--- source/parser/csv.cpp
+++ source/parser/csv.cpp
@@ -25,8 +25,143 @@
 
 #include "csv.hpp"
 
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
 
 namespace Ariadne {
 
+CSVField::CSVField(std::string field, const ParserType &type, size_t col_index)
+    : _field{field}
+    , _type{type}
+    , _col_index{col_index}
+{
+
+}
+
+CSVRow::CSVRow(std::string line, size_t size, 
+    std::vector<ParserType> &types, char separator)
+    : _line{line}
+    , _size{size}
+    , _types{types}
+    , _separator{separator}
+{
+
+}
+
+CSVRow::CSVRow(std::string line, std::vector<ParserType> &types, 
+    char separator)
+    : CSVRow{line, 
+        static_cast<size_t>(std::count(line.begin(), line.end(), separator)), 
+        types, separator}
+{
+
+}
+
+CSVRow::CSVRow(std::vector<ParserType> &types)
+    : _line{}
+    , _size{0}
+    , _types{types}
+    , _separator{}
+{
+
+}
+
+CSVRow::CSVRow(const CSVRow &obj)
+    : _line{obj._line}
+    , _size{obj._size}
+    , _types{obj._types}
+    , _separator{obj._separator}
+{
+
+}
+
+CSVField CSVRow::operator[](size_t idx) const
+{
+    if (idx >= this->_size)
+    {
+        throw std::runtime_error("operator[] failed: idx >= this->size");
+    }
+
+    std::string field;
+    size_t i = 0;
+    std::stringstream ss{_line};
+    while(std::getline(ss, field, _separator)) {
+        if(i == idx)
+        {
+            return CSVField{field, _types.at(idx), idx};
+        }
+    }
+
+    throw std::runtime_error("CSV bad format: fields missing");
+}
+
+CSVRow& CSVRow::operator=(const CSVRow &obj)
+{
+    _line = obj._line;
+    _size = obj._size;
+    _types = obj._types;
+    _separator = obj._separator;
+    return *this;
+}
+
+CSV::CSV(std::string fn, std::vector<ParserType> types, char separator) 
+    : Parser()
+    , _fn{fn}
+    , _row_header{types}
+    , _row_cache{types}
+    , _separator{separator}
+{
+    auto file = std::ifstream{fn};
+    if(!file.is_open() || !file.good()) 
+    {
+        throw std::runtime_error("Could not open file");
+    }
+
+    std::string header, first_line;
+    std::getline(file, header);
+    std::getline(file, first_line);
+
+    _row_size = static_cast<size_t>(
+        std::count(header.begin(), header.end(), separator));
+
+    if((std::find(types.begin(), types.end(), ParserType::AUTO) != types.end())
+        || types.size() == 0
+        || types.size() != _row_size) 
+    {
+        std::stringstream ss{first_line};
+        for (size_t i = 0; i < _row_size; ++i)
+        {
+            std::string s;
+            std::getline(ss, s, separator);
+            _types.push_back(parse(s));
+        }
+    }
+    else 
+    {
+        _types = types;
+    }
+
+    _row_header = CSVRow{header,     _row_size, _types, separator};
+    _row_cache  = CSVRow{first_line, _row_size, _types, separator};
+    file.close();
+}
+
+const CSVRow &CSV::operator[](size_t idx)
+{
+    auto file = std::ifstream{_fn};
+
+    for (size_t i = 0; i < idx - 1; ++i)
+    {
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    std::string line;
+    std::getline(file, line);
+    _row_cache = CSVRow{line, _row_size, _types, _separator};
+    return _row_cache;
+}
 
 } // namespace Ariadne
--- source/parser/csv.hpp
+++ source/parser/csv.hpp
@@ -29,8 +29,93 @@
 #ifndef ARIADNE_PARSER_CSV_HPP
 #define ARIADNE_PARSER_CSV_HPP
 
+#include "parser.hpp"
+
+#include <fstream>
+#include <sstream>
+#include <cstddef>
+
+
 namespace Ariadne {
 
+class CSVField
+{
+public:
+    CSVField(std::string field, const ParserType &type, size_t col_index);
+    ~CSVField() = default;
+
+    template<typename T>
+    void as(T *ptr)
+    {
+        convert(this->_field, ptr);
+    }
+
+    template<typename T>
+    T as()
+    {
+        T ret;
+        convert(this->_field, &ret);
+        return ret;
+    }
+
+    const ParserType &type()  const    { return _type; }
+    size_t col_index() const    { return _col_index; }
+
+private:
+    std::string _field;
+    const ParserType &_type;
+    size_t _col_index;
+};
+
+
+class CSVRow 
+{
+public:
+    CSVRow(std::string line, size_t size, std::vector<ParserType> &types, 
+        char separator = ',');
+    CSVRow(std::string line, std::vector<ParserType> &types, 
+        char separator = ',');
+    CSVRow(std::vector<ParserType> &types);
+    CSVRow(const CSVRow &obj);
+    ~CSVRow() = default;
+
+    // CSVField& operator[](size_t idx);
+    CSVField operator[](size_t idx) const;
+    CSVRow& operator=(const CSVRow &obj);
+
+    size_t size() const { return _size; }
+    bool empty() const { return _size == 0; }
+    const std::vector<ParserType> &types() const { return _types; } 
+
+private:
+    std::string _line;
+    size_t _size;
+    std::vector<ParserType> &_types;
+    char _separator;
+};
+
+
+class CSV : public Parser
+{
+public:
+    CSV(std::string fn, std::vector<ParserType> types = { ParserType::AUTO }, 
+        char separator = ',');
+    ~CSV() = default;
+
+    size_t row_size() const { return _row_size; }
+    const CSVRow &header() const { return _row_header; }
+    const std::vector<ParserType> &types() const { return _types; }
+
+    const CSVRow &operator[](size_t idx);
+
+private:
+    std::string _fn;
+    std::vector<ParserType> _types;
+    CSVRow _row_header;
+    CSVRow _row_cache;
+    size_t _row_size;
+    char _separator;
+};
 
 
 } // namespace Ariadne
