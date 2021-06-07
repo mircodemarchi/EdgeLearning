fd23b2ab37dc0561dd162281b4eb87ee5efb8877
Mirco De Marchi <mirco.demarchi@studenti.univr.it>
2021-06-07 11:21:12 +0200

    Add new module dnn for deep neural network classes

---
+++ source/dnn/layer.hpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *            layer.hpp
+ *
+ *  Copyright  2021  Mirco De Marchi
+ *
+ ****************************************************************************/
+
+/*
+ *  This file is part of Ariadne.
+ *
+ *  Ariadne is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Ariadne is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Ariadne.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*! \file layer.hpp
+ *  \brief High level layer of a deep neural network.
+ */
+
+#ifndef ARIADNE_DNN_LAYER_HPP
+#define ARIADNE_DNN_LAYER_HPP
+
+namespace Ariadne {
+
+
+
+} // namespace Ariadne
+
+#endif // ARIADNE_DNN_LAYER_HPP
