8acbbee3864eb122d1f520dbd016611ef441caa5
Mirco De Marchi <mircodemarchi@outlook.com>
2021-12-21 16:14:52 +0100

    Implement tests for parser

--- tests/parser/test_parser.cpp
+++ tests/parser/test_parser.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *            tests/test_parser.cpp
+ *            tests/parser.cpp
  *
  *  Copyright  2021  Mirco De Marchi
  *
@@ -25,10 +25,6 @@
 #include "test.hpp"
 #include "parser/parser.hpp"
 
-#include <vector>
-#include <map>
-#include <string>
-
 
 using namespace std;
 using namespace EdgeLearning;
@@ -36,69 +32,12 @@ using namespace EdgeLearning;
 class TestParser {
 public:
     void test() {
-        EDGE_LEARNING_TEST_CALL(test_parse());
-        EDGE_LEARNING_TEST_CALL(test_is());
-        EDGE_LEARNING_TEST_CALL(test_convert());
+        EDGE_LEARNING_TEST_CALL(test_parser());
     }
 
 private:
-    void test_parse() {
-        auto parser = Parser();
-        auto test_vec = std::map<std::string, ParserType>{
-            { "1.2",           ParserType::FLOAT  },
-            { "+0.0",          ParserType::FLOAT  },
-            { "-0.0",          ParserType::FLOAT  },
-            { "+1e-10",        ParserType::FLOAT  },
-            { "true",          ParserType::BOOL   },
-            { "1",             ParserType::INT    },
-            { "-1",            ParserType::INT    },
-            { "+0",            ParserType::INT    },
-            { "-0",            ParserType::INT    },
-            { "\"string\"",    ParserType::STRING },
-            { "123edgelearning456", ParserType::STRING },
-            { std::string{},   ParserType::NONE   },
-        };
-
-        auto keys = std::vector<std::string>{};
-        auto values = std::vector<ParserType>{};
-        for (const auto& [key, value]: test_vec)
-        {
-            keys.push_back(key);
-            values.push_back(value);
-        }
-
-        auto parsed_values = parser(keys);
-        EDGE_LEARNING_TEST_ASSERT(parsed_values == values);
-        EDGE_LEARNING_TEST_PRINT(parsed_values);
-
-        auto values_cpy = std::vector<ParserType>(values);
-        values_cpy.push_back(ParserType::NONE);
-        EDGE_LEARNING_TEST_ASSERT(parsed_values != values_cpy);
-    }
-
-    void test_is() {
-        EDGE_LEARNING_TEST_ASSERT(Parser::is_float("-0.3"));
-        EDGE_LEARNING_TEST_ASSERT(Parser::is_float(".245"));
-        EDGE_LEARNING_TEST_ASSERT(Parser::is_bool("false"));
-        EDGE_LEARNING_TEST_ASSERT(Parser::is_int("1234"));
-        EDGE_LEARNING_TEST_ASSERT(Parser::is_string("edgelearning123dl"));
-    }
-
-    void test_convert() {
-        float f;
-        EDGE_LEARNING_TEST_ASSERT(convert("1.2", &f));
-        EDGE_LEARNING_TEST_WITHIN(f, 1.2, 0.0000001);
-        bool b;
-        EDGE_LEARNING_TEST_ASSERT(convert("true", &b));
-        EDGE_LEARNING_TEST_EQUAL(b, true);
-        int i;
-        EDGE_LEARNING_TEST_ASSERT(convert("1", &i));
-        EDGE_LEARNING_TEST_EQUAL(i, 1);
-        std::string s;
-        EDGE_LEARNING_TEST_ASSERT(convert("1", &s));
-        EDGE_LEARNING_TEST_EQUAL(s, "1");
-
-        EDGE_LEARNING_TEST_ASSERT(!convert("1string", &i));
+    void test_parser() {
+        EDGE_LEARNING_TEST_CALL(Parser());
     }
 };
 
