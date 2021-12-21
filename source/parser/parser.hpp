9a7fe549daf1dde80b442f1898d57cd468fcf404
Mirco De Marchi <mircodemarchi@outlook.com>
2021-12-21 16:09:41 +0100

    Implement parser as generic abstract class

--- source/parser/parser.hpp
+++ source/parser/parser.hpp
@@ -26,99 +26,23 @@
  *  \brief Simply replace me.
  */
 
-#ifndef EDGE_LEARNING_REPLACEME_HPP
-#define EDGE_LEARNING_REPLACEME_HPP
+#ifndef EDGE_LEARNING_PARSER_PARSER_HPP
+#define EDGE_LEARNING_PARSER_PARSER_HPP
 
-
-#include <string>
-#include <sstream>
-#include <vector>
-#include <regex>
+#include "type_checker.hpp"
 
 namespace EdgeLearning {
 
-enum class ParserType : int
-{
-    NONE    = -1, 
-    AUTO    =  0,
-    FLOAT   =  1,
-    INT     =  2,
-    BOOL    =  3,
-    STRING  =  4,
-    OBJECT  =  5,
-};
-
-class Parser
+class Parser 
 {
 public:
-    Parser() {};
-    virtual ~Parser() = default;
-    
-    static bool is_float(std::string in)  
-        { return parse(in) == ParserType::FLOAT;  };
-    static bool is_bool(std::string in)   
-        { return parse(in) == ParserType::BOOL;   };
-    static bool is_int(std::string in)    
-        { return parse(in) == ParserType::INT;    };
-    static bool is_string(std::string in) 
-        { return parse(in) == ParserType::STRING; };
-    
-    virtual ParserType operator()(const std::string &field) const 
-    {
-        return parse(field);
-    }
-
-    virtual std::vector<ParserType> operator()(
-        const std::vector<std::string> &fields) const 
-    {
-        return parse(fields);
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Parser& obj);
+    Parser() : _tc() {}
+    virtual ~Parser() {};
 
 protected:
-    static ParserType parse(const std::string &field);
-    static std::vector<ParserType> parse(
-        const std::vector<std::string> &fields);
-
-private:
-    static const std::regex _float_regex;
-    static const std::regex _boolean_regex;
-    static const std::regex _integer_regex;
-    static const std::regex _string_regex;
+    TypeChecker _tc;
 };
-
-std::ostream& operator<<(std::ostream& os, const ParserType& obj);
-std::ostream& operator<<(std::ostream& os, const std::vector<ParserType>& obj);
-
-bool operator==(const std::vector<ParserType> &lhs, 
-    const std::vector<ParserType> &rhs);
-bool operator!=(const std::vector<ParserType> &lhs, 
-    const std::vector<ParserType> &rhs);
-
-template<typename T>
-bool convert(const std::string &s, T *ptr)
-{
-    if constexpr (std::is_same_v<T, bool>)
-    {
-        if (s == "true" || s == "1")
-        {
-            *ptr = true;
-        }
-        else 
-        {
-            *ptr = false;
-        }
-
-        return true;
-    }
-
-    std::stringstream ss{s};
-    ss >> *ptr;
-
-    return !ss.fail() && ss.eof();
-}
 
 } // namespace EdgeLearning
 
-#endif // EDGE_LEARNING_REPLACEME_HPP
+#endif // EDGE_LEARNING_PARSER_PARSER_HPP
