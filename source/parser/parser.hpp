37449a6b1d441ae2ae10ddbd6770f39d4d95add1
Mirco De Marchi <mircodemarchi@outlook.com>
2022-09-13 10:51:06 +0200

    Implement and test DatasetParser abstract class

--- source/parser/parser.hpp
+++ source/parser/parser.hpp
@@ -30,6 +30,11 @@
 #define EDGE_LEARNING_PARSER_PARSER_HPP
 
 #include "type_checker.hpp"
+#include "type.hpp"
+
+#include <set>
+#include <map>
+
 
 namespace EdgeLearning {
 
@@ -47,10 +52,180 @@ public:
     /**
      * \brief Destroy the Parser object.
      */
-    virtual ~Parser() {};
+    virtual ~Parser() = default;
 
 protected:
     TypeChecker _tc; ///< TypeChecker entity to perform parsing and convertion.
+};
+
+
+class DatasetParser : public Parser
+{
+public:
+
+    enum class LabelEncoding {
+        ONE_HOT_ENCODING,
+        DEFAULT_ENCODING
+    };
+
+    using OneHotLabelMap = std::map<NumType, SizeType>;
+
+    DatasetParser()
+        : Parser()
+    { }
+
+    virtual ~DatasetParser() = default;
+
+    virtual std::vector<NumType> entry(SizeType i) = 0;
+    virtual SizeType entries_amount() const = 0;
+    virtual SizeType feature_size() const = 0;
+    virtual std::set<SizeType> labels_idx() const = 0;
+
+    std::set<NumType> unique(SizeType idx)
+    {
+        std::vector<NumType> ret;
+
+        for (SizeType i = 0; i < entries_amount(); ++i)
+        {
+            ret.push_back(entry(i)[idx]);
+        }
+
+        return {ret.begin(), ret.end()};
+    }
+
+    OneHotLabelMap unique_map(SizeType idx)
+    {
+        auto unique_set = unique(idx);
+        OneHotLabelMap value_to_index;
+
+        SizeType i = 0;
+        for (const auto& l: unique_set)
+        {
+            value_to_index[l] = i++;
+        }
+
+        return value_to_index;
+    }
+
+    SizeType encoding_feature_size(
+        LabelEncoding label_encoding = LabelEncoding::DEFAULT_ENCODING)
+    {
+        switch(label_encoding)
+        {
+            case LabelEncoding::ONE_HOT_ENCODING:
+            {
+                SizeType entry_size = feature_size() - labels_idx().size();
+                for (const auto& label_idx: labels_idx())
+                {
+                    auto one_hot_label_map = unique(label_idx);
+                    entry_size += one_hot_label_map.size();
+                }
+                return entry_size;
+            }
+            case LabelEncoding::DEFAULT_ENCODING:
+            default:
+            {
+                return feature_size();
+            }
+        }
+    }
+
+    std::set<SizeType> encoding_labels_idx(
+        LabelEncoding label_encoding = LabelEncoding::DEFAULT_ENCODING)
+    {
+        switch(label_encoding)
+        {
+            case LabelEncoding::ONE_HOT_ENCODING:
+            {
+                std::set<SizeType> encoding_label_indexes;
+                auto encoding_entry_size = encoding_feature_size(label_encoding);
+                SizeType trainset_idx_offset = feature_size() - labels_idx().size();
+                for (SizeType i = trainset_idx_offset; i < encoding_entry_size; ++i)
+                {
+                    encoding_label_indexes.insert(i);
+                }
+                return encoding_label_indexes;
+            }
+            case LabelEncoding::DEFAULT_ENCODING:
+            default:
+            {
+                return labels_idx();
+            }
+        }
+    }
+
+    std::vector<NumType> data_to_encoding(
+        LabelEncoding label_encoding = LabelEncoding::DEFAULT_ENCODING)
+    {
+        switch(label_encoding)
+        {
+            case LabelEncoding::ONE_HOT_ENCODING:
+            {
+                auto label_indexes = labels_idx();
+                std::vector<OneHotLabelMap> one_hot_label_maps;
+                SizeType entry_size = feature_size() - label_indexes.size();
+                for (const auto& label_idx: label_indexes)
+                {
+                    auto one_hot_label_map = unique_map(label_idx);
+                    one_hot_label_maps.push_back(one_hot_label_map);
+                    entry_size += one_hot_label_map.size();
+                }
+
+                std::vector<NumType> ret(entry_size * entries_amount());
+                std::vector<SizeType> label_indexes_vec(
+                    label_indexes.begin(), label_indexes.end());
+                for (SizeType row_idx = 0; row_idx < entries_amount(); ++row_idx)
+                {
+                    auto row = entry(row_idx);
+
+                    // Copy the trainset.
+                    for (SizeType col_idx = 0; col_idx < feature_size(); ++col_idx)
+                    {
+                        if (label_indexes.find(col_idx) == label_indexes.end())
+                        {
+                            ret[row_idx * entry_size + col_idx] = row[col_idx];
+                        }
+                    }
+
+                    // Copy the one hot vector for each label.
+                    SizeType col_offset = feature_size() - label_indexes.size();
+                    for (SizeType i = 0; i < label_indexes_vec.size(); ++i)
+                    {
+                        const OneHotLabelMap& one_hot_label_map = one_hot_label_maps[i];
+                        auto label_value = row[label_indexes_vec[i]];
+
+                        // Init the one hot label vector.
+                        auto one_hot_size = one_hot_label_map.size();
+                        auto one_hot_idx = one_hot_label_map.at(label_value);
+                        for(SizeType col_idx = 0; col_idx < one_hot_size; ++col_idx)
+                        {
+                            ret[row_idx * entry_size + col_offset + col_idx] =
+                                col_idx == one_hot_idx ? NumType(1) : NumType(0);
+                        }
+
+                        col_offset += one_hot_size;
+                    }
+                }
+                return ret;
+            }
+            case LabelEncoding::DEFAULT_ENCODING:
+            default:
+            {
+                std::vector<NumType> ret(feature_size() * entries_amount());
+                for (SizeType row_idx = 0; row_idx < entries_amount(); ++row_idx)
+                {
+                    auto row = entry(row_idx);
+                    for (SizeType col_idx = 0; col_idx < feature_size(); ++col_idx)
+                    {
+                        ret[row_idx * feature_size() + col_idx] = row[col_idx];
+                    }
+                }
+                return ret;
+            }
+        }
+    }
+
+
 };
 
 } // namespace EdgeLearning
--- tests/parser/test_parser.cpp
+++ tests/parser/test_parser.cpp
@@ -25,19 +25,160 @@
 #include "test.hpp"
 #include "parser/parser.hpp"
 
+#include <algorithm>
+
 
 using namespace std;
 using namespace EdgeLearning;
+
+
+class ExampleDatasetParser : public DatasetParser
+{
+public:
+    ExampleDatasetParser(const std::vector<NumType>& data,
+                         SizeType feature_size,
+                         std::set<SizeType> labels_idx = {})
+        : _data(data)
+        , _feature_size(feature_size)
+        , _entries_amount(data.size() / std::max(feature_size, SizeType(1)))
+        , _labels_idx(labels_idx)
+    {
+
+    }
+
+    std::vector<NumType> entry(SizeType i) override {
+        if (i >= _entries_amount) return {};
+
+        std::vector<NumType> ret(_feature_size);
+        auto offset = static_cast<std::int64_t>(i * _feature_size);
+        std::copy(_data.begin() + offset,
+                  _data.begin() + offset + static_cast<std::int64_t>(_feature_size),
+                  ret.begin());
+        return ret;
+    }
+
+    SizeType entries_amount() const override {
+        return _entries_amount;
+    }
+
+    SizeType feature_size() const override {
+        return _feature_size;
+    }
+
+    std::set<SizeType> labels_idx() const override {
+        return _labels_idx;
+    }
+
+    std::vector<NumType> _data;
+    SizeType _feature_size;
+    SizeType _entries_amount;
+    std::set<SizeType> _labels_idx;
+};
+
 
 class TestParser {
 public:
     void test() {
         EDGE_LEARNING_TEST_CALL(test_parser());
+        EDGE_LEARNING_TEST_CALL(test_dataset_parser());
     }
 
 private:
     void test_parser() {
-        EDGE_LEARNING_TEST_CALL(Parser());
+        EDGE_LEARNING_TEST_TRY(Parser());
+    }
+
+    void test_dataset_parser() {
+        std::vector<NumType> v({
+            5,5,5,5,5,0,1,
+            5,5,5,5,5,1,1,
+            5,5,5,5,5,0,2,
+            5,5,5,5,5,1,3,
+            5,5,5,5,5,0,1,
+            5,5,5,5,5,0,1,
+        });
+        std::set<SizeType> v_idx({5,6});
+        SizeType feature_size = 7;
+
+        EDGE_LEARNING_TEST_TRY(ExampleDatasetParser(v, feature_size));
+        auto edp = ExampleDatasetParser(v, feature_size);
+        EDGE_LEARNING_TEST_EQUAL(edp.labels_idx().size(), 0);
+
+        EDGE_LEARNING_TEST_TRY(ExampleDatasetParser(v, feature_size, v_idx));
+        edp = ExampleDatasetParser(v, feature_size, v_idx);
+        EDGE_LEARNING_TEST_EQUAL(edp.feature_size(), feature_size);
+        EDGE_LEARNING_TEST_EQUAL(edp.entries_amount(), 6);
+        EDGE_LEARNING_TEST_EQUAL(edp.entry(0).size(), feature_size);
+        EDGE_LEARNING_TEST_EQUAL(edp.entry(1).size(), feature_size);
+        EDGE_LEARNING_TEST_EQUAL(edp.entry(0)[0], 5);
+        EDGE_LEARNING_TEST_EQUAL(edp.entry(edp.entries_amount()).size(), 0);
+        EDGE_LEARNING_TEST_EQUAL(edp.labels_idx().size(), 2);
+
+        EDGE_LEARNING_TEST_TRY((void) edp.unique(5));
+        auto label5_values = edp.unique(5);
+        for (const auto& val: label5_values)
+        {
+            EDGE_LEARNING_TEST_PRINT(val);
+        }
+        EDGE_LEARNING_TEST_ASSERT(label5_values.count(0) > 0);
+        EDGE_LEARNING_TEST_ASSERT(label5_values.count(1) > 0);
+        EDGE_LEARNING_TEST_ASSERT(label5_values.count(2) == 0);
+
+        EDGE_LEARNING_TEST_TRY((void) edp.unique_map(6));
+        auto label6_values_map = edp.unique_map(6);
+        for (const auto& val: label6_values_map)
+        {
+            EDGE_LEARNING_TEST_PRINT(val.first);
+            EDGE_LEARNING_TEST_PRINT(val.second);
+        }
+        EDGE_LEARNING_TEST_ASSERT(label6_values_map.count(0) == 0);
+        EDGE_LEARNING_TEST_ASSERT(label6_values_map.count(1) > 0);
+        EDGE_LEARNING_TEST_ASSERT(label6_values_map.count(2) > 0);
+        EDGE_LEARNING_TEST_ASSERT(label6_values_map.count(3) > 0);
+        EDGE_LEARNING_TEST_ASSERT(label6_values_map.count(4) == 0);
+        EDGE_LEARNING_TEST_EQUAL(label6_values_map[1], 0);
+        EDGE_LEARNING_TEST_EQUAL(label6_values_map[2], 1);
+        EDGE_LEARNING_TEST_EQUAL(label6_values_map[3], 2);
+
+        EDGE_LEARNING_TEST_TRY((void) edp.data_to_encoding(
+            ExampleDatasetParser::LabelEncoding::DEFAULT_ENCODING));
+        auto data_default_encoding = edp.data_to_encoding(
+            ExampleDatasetParser::LabelEncoding::DEFAULT_ENCODING);
+        EDGE_LEARNING_TEST_EQUAL(v.size(), data_default_encoding.size());
+        EDGE_LEARNING_TEST_EQUAL(
+            edp.encoding_feature_size(
+                ExampleDatasetParser::LabelEncoding::DEFAULT_ENCODING),
+                edp.feature_size());
+        EDGE_LEARNING_TEST_EQUAL(
+            edp.encoding_labels_idx(
+                ExampleDatasetParser::LabelEncoding::DEFAULT_ENCODING).size(),
+                edp.labels_idx().size());
+        for (SizeType i = 0; i < v.size(); ++i)
+        {
+            EDGE_LEARNING_TEST_EQUAL(v[i], data_default_encoding[i]);
+        }
+
+        EDGE_LEARNING_TEST_TRY((void) edp.data_to_encoding(
+            ExampleDatasetParser::LabelEncoding::ONE_HOT_ENCODING));
+        auto data_one_hot_encoding = edp.data_to_encoding(
+            ExampleDatasetParser::LabelEncoding::ONE_HOT_ENCODING);
+        EDGE_LEARNING_TEST_EQUAL(edp.encoding_feature_size(
+                ExampleDatasetParser::LabelEncoding::ONE_HOT_ENCODING), 5+2+3);
+        EDGE_LEARNING_TEST_EQUAL(edp.encoding_labels_idx(
+            ExampleDatasetParser::LabelEncoding::ONE_HOT_ENCODING).size(), 2+3);
+
+        std::vector<NumType> test_v({
+           5,5,5,5,5,1,0,1,0,0,
+           5,5,5,5,5,0,1,1,0,0,
+           5,5,5,5,5,1,0,0,1,0,
+           5,5,5,5,5,0,1,0,0,1,
+           5,5,5,5,5,1,0,1,0,0,
+           5,5,5,5,5,1,0,1,0,0,
+        });
+        for (SizeType i = 0; i < test_v.size(); ++i)
+        {
+            EDGE_LEARNING_TEST_EQUAL(data_one_hot_encoding[i], test_v[i]);
+        }
     }
 };
 
